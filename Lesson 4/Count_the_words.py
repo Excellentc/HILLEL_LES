cb4fe82b6e8de32187878b5c29b119ecce4f8c51
Denis Rudomotkin <kochapp@gmail.com>
2020-06-16 00:17:03 +0300

    Исправлены мелкие ошибки

--- Lesson 4/Count_the_words.py
+++ Lesson 4/Count_the_words.py
@@ -1,2 +1,2 @@
 user_input = str(input("Введите несколько слов через пробел : "))
-print("Количество слов в предложении",user_input.strip(" ").count(" ")+1)
+print("Количество слов в предложении", user_input.strip(" ").count(" ")+1)
