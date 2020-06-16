d7dd979e0ed19590401047612bd5e2c8ecea2f68
Denis Rudomotkin <kochapp@gmail.com>
2020-06-16 10:36:30 +0300

    пробуем удалить папку

--- Lesson_4/DZ_4_letter replacement.py
+++
@@ -1,5 +0,0 @@
-user_input = str(input("Введиет Вашу строку : "))
-x1 = int(user_input.find("h")+1)
-x2 = int(user_input.rfind("h"))
-y = user_input[x1:x2:]
-print(user_input[:x1:]+y.replace("h", "H", len(y)+1)+user_input[x2::])
