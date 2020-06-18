824fad982e532c981be42b0a3fd8380c66b2f7b8
Denis Rudomotkin <kochapp@gmail.com>
2020-06-18 10:20:46 +0300

    проверка на количество букв H > 0

--- Lesson 4/DZ_4_letter replacement.py
+++ Lesson 4/DZ_4_letter replacement.py
@@ -1,5 +1,10 @@
-user_input = str(input("Введиет Вашу строку : "))
+while 1 == 1:
+    user_input = str(input("Введиет Вашу строку : "))
+    if user_input.count("h") > 1:
+        break
+    else:
+        print("Повторите ввод пожалуйста")
 x1 = int(user_input.find("h")+1)
 x2 = int(user_input.rfind("h"))
 y = user_input[x1:x2:]
-print(user_input[:x1:]+y.replace("h", "H", len(y)+1)+user_input[x2::])
+print("\t\t\t\t\t  "+user_input[:x1:]+y.replace("h", "H", len(y)+1)+user_input[x2::])
