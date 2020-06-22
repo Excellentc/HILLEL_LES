c6c642f93a779b7db21e674afea336a0cbf9ff1c
Denis Rudomotkin <kochapp@gmail.com>
2020-06-22 18:56:10 +0300

    Новое решение без IF  и проверки на нулевое значение

--- Lesson 3/DZ_3_class_room.py
+++ Lesson 3/DZ_3_class_room.py
@@ -1,15 +1,6 @@
+
 summ = 0
 for i in range(3):
-    while 1 == 1:
-        klas_pip = int(input(f"Введите количество учеников в классе {i+1} : "))
-        if klas_pip >= 0:
-            break
-        else:
-            print("Количество учеников не должно быть отрицательным\n")
-    if klas_pip == 0:
-        summ = summ
-    elif klas_pip % 2 == 0:
-        summ = summ + klas_pip / 2
-    else:
-        summ = summ + klas_pip // 2 +1
+    klas_piple = int(input(f"Введите количество учеников в классе {i+1} : "))
+    summ = summ + (klas_piple + klas_piple%2)/2
 print("Общее количество парт", summ)
