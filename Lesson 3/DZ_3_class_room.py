7cc5668e0132917770f52e015c6177bc9eb8039f
Denis Rudomotkin <kochapp@gmail.com>
2020-06-29 16:20:39 +0300

    мелочи

--- Lesson 3/DZ_3_class_room.py
+++ Lesson 3/DZ_3_class_room.py
@@ -2,5 +2,5 @@
 summ = 0
 for i in range(3):
     klas_piple = int(input(f"Введите количество учеников в классе {i+1} : "))
-    summ = summ + (klas_piple + klas_piple%2)/2
+    summ = summ + (klas_piple + klas_piple % 2)/2
 print("Общее количество парт", summ)
