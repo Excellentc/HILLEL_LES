886e52c417b063d992858fa81733cb8c549bc7fd
Denis Rudomotkin <kochapp@gmail.com>
2020-08-18 21:01:51 +0300

    Исправлены ошибки: "Вы динамическим создаёте атрибуты класса, вместо инициализации объекта Student при его создании. Соответственно неверно пытаетесь добавить студента в группу.(c)"

--- Lesson 14/DZ_28.py
+++ Lesson 14/DZ_28.py
@@ -8,44 +8,34 @@
 
 class Student:
 
-    def __init__(self, name="", age=0, grade=0):
+    def __init__(self, name, age, grade):
         self.name = name
         self.age = age
         self.grade = grade
 
 
 class Group:
+    def __init__(self, student_list):
+        self.st_list = student_list
 
-    def __init__(self, group):
-        self.group = group
+    def student_add(self, x, y, c):
+        self.st_list.append((x, y, c))
 
-    def st_add(self):
-        Student.name = str(input("Enter student name : "))
-        Student.age = int(input("Enter student age : "))
-        Student.grade = int(input("Enter student grade : "))
-
-    def group_st(self):     # Добавление студента
-        d = []
-        d.append(Student.name)
-        d.append(Student.age)
-        d.append(Student.grade)
-        self.group.append(d)
-
-    def group_pr(self):     # Print group
-        if len(self.group) == 0:
+    def group_print(self):     # Print group
+        if len(self.st_list) == 0:
             print("Group is empty")
-        for i in range(len(self.group)):
-            for j in range(len(self.group[i])):
-                print('{:10}'.format(self.group[i][j]).rstrip("/n"), end="")
+        for i in range(len(self.st_list)):
+            for j in range(len(self.st_list[i])):
+                print('{:10}'.format(self.st_list[i][j]).rstrip("/n"), end="")
             print()
 
-    def st_del(self, name):     # Delete student by name
-        for i in range(len(self.group)):
-            if name in self.group[i]:
-                return self.group.pop(i)
+    def student_del(self, name):     # Delete student by name
+        for i in range(len(self.st_list)):
+            if name in self.st_list[i]:
+                return self.st_list.pop(i)
 
 
-list1 = []
+l1 = []
 while 1 == 1:
     ch = int(input("Make choice:" "\n"
                    " List group - 1 : ""\n"
@@ -54,19 +44,25 @@ while 1 == 1:
                    " Quit - 4 : "))
     print()
     if ch == 1:     # List group
-        x = Group(list1)
-        x.group_pr()
+        x = Group(l1)
+        x.group_print()
         print()
+
     if ch == 2:     # Add student
-        x = Group(list1)
-        x.st_add()
-        x.group_st()
+        n1 = str(input("Enter student name : "))
+        a1 = int(input("Enter student age : "))
+        g1 = int(input("Enter student grade : "))
+        stu = Student(n1, a1, g1)
+        group_st = Group(l1)
+        group_st.student_add(stu.name, stu.age, stu.grade)
         print()
+
     if ch == 3:     # Delete student
         del_student = str(input("Enter name student for delete : "))
-        x = Group(list1)
-        y = x.st_del(del_student)
+        x = Group(l1)
+        y = x.student_del(del_student)
         print(del_student, "is delete")
         print()
     if ch == 4:     # Quit
+        print("You are quit")
         break
